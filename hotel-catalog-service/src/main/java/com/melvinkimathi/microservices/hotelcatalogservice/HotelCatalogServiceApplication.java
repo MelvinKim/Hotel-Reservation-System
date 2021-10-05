07de018ca4e879317cb2b4e0852f3148f6e7e9c9
Melvin Kimathi <53833444+MelvinKim@users.noreply.github.com>
2021-10-05 17:04:47 +0300

    add sql code for database creation and configuration

---
+++ hotel-catalog-service/src/main/java/com/melvinkimathi/microservices/hotelcatalogservice/HotelCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.melvinkimathi.microservices.hotelcatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ hotel-catalog-service/src/test/java/com/melvinkimathi/microservices/hotelcatalogservice/HotelCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melvinkimathi.microservices.hotelcatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
